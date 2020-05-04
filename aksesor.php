0f333721248242bcec055356590fa0787d5b1e8a
rinesam <58374578+rinesam@users.noreply.github.com>
2020-05-04 12:38:33 +0200

    ndryshime

---
+++ aksesor.php
@@ -0,0 +1,19 @@
+<?php
+require_once('./php/component.php');
+?>
+
+<!DOCTYPE html>
+
+<html lang="en" dir="ltr">
+  <head>
+    <title>Lejleku - Per ne</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/perne.css">
+    <link rel="icon" href="images/logo-bar.png" type="image/x-icon">
+  </head>
+  <body>
+      <?php require_once("components/header.php"); ?>
+            <!-- #######################################MENYT KRYESORE################################################### -->
+            <!-- ########################################################################################## -->
+<div>
+</html>
