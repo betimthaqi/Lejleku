a059882def5b25a197aefdb1bfe483a66a770052
Durajet Mustafa <durajet.mustafa@studentet.uni-pr.edu>
2020-05-11 15:47:51 +0200

    added: company filter

--- components/pricerange.php
+++ components/pricerange.php
@@ -13,7 +13,8 @@ if (!empty($_POST['max_price'])) {
     $max = $_POST['max_price'];
 }
 
-$product_ofertaResult = runQuery("SELECT DISTINCT product_oferta FROM Allproducts ORDER BY product_oferta ASC");
+
+
 function runQuery($query)
 {
     global $conn;
@@ -24,48 +25,62 @@ function runQuery($query)
     if (!empty($resultset))
         return $resultset;
 }
-?>
-<div class="box">
-    <a href="#" class="btn btn-white btn-animation-1" onclick="myFunction()"><img src="images/filterpng.png" width="40px" height="40px" alt=""></a>
+
+function filtro($tabela)
+{
+    global $min;
+    global $max;
+    $product_ofertaResult = runQuery("SELECT DISTINCT product_oferta FROM Allproducts ORDER BY product_oferta ASC");
+    $product_companyResult = runQuery("SELECT DISTINCT product_company FROM Allproducts WHERE product_category = '$tabela' ORDER BY product_company ASC");
+
+    echo "<div class=\"box\">
+    <a href=\"#\" class=\"btn btn-white btn-animation-1\" onclick=\"myFunction()\"><img src=\"images/filterpng.png\" width=\"40px\" height=\"40px\" alt=\"\"></a>
 </div>
-<div id="demo" style="display: none;">
-    <div class="container popuptext" id="myPopup">
-        <div class="form-price-range-filter">
-            <form id="filter-form" name="search" class="filter-form" method="POST">
-                <div class="form-group">
-                    <input type="" id="min" name="min_price" value="<?php echo $min . "€"; ?>" readonly="readonly">
-                    <div id="slider-range"></div>
-                    <input type="" id="max" name="max_price" value="<?php echo $max . "€"; ?>" readonly="readonly">
+<div id=\"demo\" style=\"display: none;\">
+    <div class=\"container popuptext\" id=\"myPopup\">
+        <div class=\"form-price-range-filter\">
+            <form id=\"filter-form\" name=\"search\" class=\"filter-form\" method=\"POST\">
+                <div class=\"form-group\">
+                    <input type=\"\" id=\"min\" name=\"min_price\" value=\"$min\" readonly=\"readonly\">
+                    <div id=\"slider-range\"></div>
+                    <input type=\"\" id=\"max\" name=\"max_price\" value=\"$max\" readonly=\"readonly\">
                 </div>
 
-                <!-- aabadabdaabagqeqfeqfed -->
-                <div class="dropdownlist">
-                    <select id="Place" name="product_oferta[]" multiple="multiple">
-                        <option value='0'>Zgjedhni produktin:</option>
-                        <?php
-                        if (!empty($product_ofertaResult)) {
-                            foreach ($product_ofertaResult as $key => $value) {
-                                echo '<option value="' . $product_ofertaResult[$key]['product_oferta'] . '">' . $product_ofertaResult[$key]['product_oferta'] . '</option>';
-                            }
-                        }
-                        ?>
-                    </select>
-                </div>
-                <div class="form-submit">
-                    <input type="submit" id="submit" class="submit" value="Filtro" />
-                </div>
-            </form>
-            <!-- ashfoafaijfaaj -->
-        </div>
-    </div>
+                <div class=\"dropdownlist\">
+                    <select id=\"Place\" name=\"product_oferta[]\" multiple=\"multiple\">";
+    if (!empty($product_ofertaResult)) {
+        foreach ($product_ofertaResult as $key => $value) {
+            echo '<option value="' . $product_ofertaResult[$key]['product_oferta'] . '">' . $product_ofertaResult[$key]['product_oferta'] . '</option>';
+        }
+    }
+    echo "</select>
+</div>;
+                <div class=\"dropdownlist\">
+                    <select id=\"Place\" name=\"product_company[]\" multiple=\"multiple\">";
+    if (!empty($product_companyResult)) {
+        foreach ($product_companyResult as $key => $value) {
+            echo '<option value="' . $product_companyResult[$key]['product_company'] . '">' . $product_companyResult[$key]['product_company'] . '</option>';
+        }
+    }
+    echo "</select>
 </div>
-<?php
+<div class=\"form-submit\">
+    <input type=\"submit\" id=\"submit\" class=\"submit\" value=\"Filtro\" />
+</div>
+</form>
+</div>
+</div>
+</div>";
+}
+
 function pricerange($tabela, $faqja)
 {
     global $min;
     global $max;
     global $conn;
-    
+
+    filtro($tabela);
+
     if (!empty($_POST['product_oferta'])) {
         $i = 0;
         $selectedOptionCount = count($_POST['product_oferta']);
@@ -83,19 +98,42 @@ function pricerange($tabela, $faqja)
 
         if (!empty($result)) {
             foreach ($result as $key => $value) {
-                component($result[$key]['product_name'], $result[$key]['product_price'], $result[$key]['product_image'], $result[$key]['id'], $result[$key]['product_ofertangjyra'], $result[$key]['product_oferta'], $result[$key]['product_category'], $result[$key]['product_description'], $result[$key]['product_earlierprice']);
+                component($result[$key]['product_name'], $result[$key]['product_price'], $result[$key]['product_image'], $result[$key]['id'], $result[$key]['product_ofertangjyra'], $result[$key]['product_oferta'], $result[$key]['product_category'], $result[$key]['product_company'], $result[$key]['product_description'], $result[$key]['product_earlierprice']);
             }
         }
         echo "<div class=\"result\" style=\"background: #ddeeff;\"><a id=\"mbrapa\" href=\"$faqja.php\"> KTHEHU NE FAQE <img src=\"images\kthehumbrapa.png\" width=\"50px\" height=\"50px\" style=\"vertical-align: middle;\"></a></div>";
         return;
     }
 
+    if (!empty($_POST['product_company'])) {
+        $i = 0;
+        $selectedOptionCount = count($_POST['product_company']);
+        $selectedOption = "";
+        while ($i < $selectedOptionCount) {
+            $selectedOption = $selectedOption . "'" . $_POST['product_company'][$i] . "'";
+            if ($i < $selectedOptionCount - 1) {
+                $selectedOption = $selectedOption . ", ";
+            }
+            $i++;
+        }
+        $query = "SELECT * from Allproducts WHERE (product_category = '$tabela' and product_price BETWEEN '$min' AND '$max' and product_company in (" . $selectedOption . "))";
+
+        $result = runQuery($query);
+
+        if (!empty($result)) {
+            foreach ($result as $key => $value) {
+                component($result[$key]['product_name'], $result[$key]['product_price'], $result[$key]['product_image'], $result[$key]['id'], $result[$key]['product_ofertangjyra'], $result[$key]['product_oferta'], $result[$key]['product_category'], $result[$key]['product_company'], $result[$key]['product_description'], $result[$key]['product_earlierprice']);
+            }
+        }
+        echo "<div class=\"result\" style=\"background: #ddeeff;\"><a id=\"mbrapa\" href=\"$faqja.php\"> KTHEHU NE FAQE <img src=\"images\kthehumbrapa.png\" width=\"50px\" height=\"50px\" style=\"vertical-align: middle;\"></a></div>";
+        return;
+    }
 
     $result1 = mysqli_query($conn, "select * from Allproducts where product_category = '$tabela' AND product_price BETWEEN '$min' AND '$max' ORDER BY product_price DESC");
     $count = mysqli_num_rows($result1);
     if ($count > 0) {
         while ($row = mysqli_fetch_array($result1)) {
-            component($row['product_name'], $row['product_price'], $row['product_image'], $row['id'], $row['product_ofertangjyra'], $row['product_oferta'], $row['product_category'], $row['product_description'], $row['product_earlierprice']);
+            component($row['product_name'], $row['product_price'], $row['product_image'], $row['id'], $row['product_ofertangjyra'], $row['product_oferta'], $row['product_category'], $row['product_company'], $row['product_description'], $row['product_earlierprice']);
         }
         echo "<div class=\"result\" style=\"background: #ddeeff;\"><a id=\"mbrapa\" href=\"$faqja.php\"> KTHEHU NE FAQE <img src=\"images\kthehumbrapa.png\" width=\"50px\" height=\"50px\" style=\"vertical-align: middle;\"></a></div>";
     } else {
@@ -107,7 +145,6 @@ function pricerange($tabela, $faqja)
 
 
 ?>
-
 
 <script>
     function myFunction() {
