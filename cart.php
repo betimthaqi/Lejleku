02352d6d1c80f88f6a7edd50aa7f3e683ca5b566
rinesam <58374578+rinesam@users.noreply.github.com>
2020-05-11 22:47:17 +0200

    card

--- cart.php
+++ cart.php
@@ -1,95 +1,127 @@
 <?php
+
 session_start();
 
-require_once("php/CreateDb.php");
-require_once("php/component.php");
+require_once ("php/CreateDb.php");
+require_once ("php/component.php");
+
 $db = new CreateDb();
-if (isset($_POST['remove'])) {
-    if ($_GET['action'] == 'remove') {
-        foreach ($_SESSION['cart'] as $key => $value) {
-            if ($value["product_id"] == $_GET['id']) {
-                unset($_SESSION['cart'][$key]);
-                echo "<script>alert('Product has been removed')</script>";
-                echo "<script>window.location = 'cart.php'</script>";
-            }
-        }
-    }
+
+if (isset($_POST['remove'])){
+  if ($_GET['action'] == 'remove'){
+      foreach ($_SESSION['cart'] as $key => $value){
+          if($value["product_id"] == $_GET['id']){
+              unset($_SESSION['cart'][$key]);
+              echo "<script>alert('Product has been Removed...!')</script>";
+              echo "<script>window.location = 'cart.php'</script>";
+          }
+      }
+  }
 }
+
+
 ?>
 
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Cart</title>
-<link rel="stylesheet" href="css/cart.css">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.css" />
+
+    <!-- Bootstrap CDN -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/layout.css">
+    <link rel="stylesheet" href="css/cart.css">
+    <link rel="stylesheet" href="css/pricerange.css">
+    <link rel="stylesheet" href="css/dropdown.css">
+    <link rel="stylesheet" href="https://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <link rel="stylesheet" href="css/cartpage.css">
 </head>
+<body class="bg-light">
 
-<body>
-<div class="row">
-  <div class="column">
-    <?php
-    require_once('components/header.php');
-    ?>
-
-    <div class="container-carta">
-        <div class="carta">
-            <?php
-            $total = 0;
-            if (isset($_SESSION['cart'])) {
-                $product_id = array_column($_SESSION['cart'], 'product_id');
-                $result = $db->getData();
-                while ($row = mysqli_fetch_assoc($result)) {
-                    foreach ($product_id as $id) {
-                        if ($row['id'] == $id) {
-                            cartElement($row['product_image'], $row['product_name'], $row['product_price'], $row['id']);
-                            $total = $total + (int) $row['product_price'];
-                        }
-                    }
-                }
-            } else {
-                echo "<h5>Cart is Empty</h5>";
-            }
-            ?>
-        </div>
-		</div>
-</div>
-<div><tr>
-<div class="summary">
-
-        <div class="total-price">
-            <div class="top"><h3>PRICE DETAILS</h3></div>
-            <hr>
-            <?php
-            if (isset($_SESSION['cart'])) {
-                $count = count($_SESSION['cart']);
-                echo "<h5>Price ($count items) $$total</h5>";
-            } else  {
-                echo "<h5>Price (0 items)</h5>";
-            }
-            ?>
-			
-            <h5>Delivery Charges</h5>
-            <hr>
-       
-
-            <?php
-            echo "<h5>Amount Payable $$total</h5>";
+<?php
+    require_once ('components/header.php');
+    headeri("","","","");
 ?>
-</div>
-    </div>
-</div>
-<?php require_once("components/footeri.php")?>
-<div class="wrapper row5">
-        <footer id="copyright" class="clear">
-            <p class="fl_left">Copyright &copy; 2020 - All Rights Reserved hahah- <a href="#">Grupi 7</a></p>
-            <p class="fl_right">Projekti ne <a target="_blank" href="https://github.com/durajetz/PHP" title="">Github</a></p>
-        </footer>
-    </div>
-   
-</body>
 
+<div class="container-fluid">
+    <div class="row px-5">
+        <div class="col-md-7">
+            <div class="shopping-cart">
+                <h6>My Cart</h6>
+                <hr>
+                <?php
+
+                $total = 0;
+                    if (isset($_SESSION['cart'])){
+                        $product_id = array_column($_SESSION['cart'], 'product_id');
+
+                        $result = $db->getData();
+                        while ($row = mysqli_fetch_assoc($result)){
+                            foreach ($product_id as $id){
+                                if ($row['id'] == $id){
+                                    cartElement($row['product_image'], $row['product_name'],$row['product_price'], $row['id']);
+                                    $total = $total + (int)$row['product_price'];
+                                }
+                            }
+                        }
+                    }else{
+                        echo "<h5>Cart is Empty</h5>";
+                    }
+
+                ?>
+
+            </div>
+        </div>
+        <div class="col-md-4 offset-md-1 border rounded mt-5 bg-white h-25">
+
+            <div class="pt-4">
+                <h6>PRICE DETAILS</h6>
+                <hr>
+                <div class="row price-details">
+                    <div class="col-md-6">
+                        <?php
+                            if (isset($_SESSION['cart'])){
+                                $count  = count($_SESSION['cart']);
+                                echo "<h6>Price ($count items)</h6>";
+                            }else{
+                                echo "<h6>Price (0 items)</h6>";
+                            }
+                        ?>
+                        <h6>Delivery Charges</h6>
+                        <hr>
+                        <h6>Amount Payable</h6>
+                    </div>
+                    <div class="col-md-6">
+                        <h6>$<?php echo $total; ?></h6>
+                        <h6 class="text-success">FREE</h6>
+                        <hr>
+                        <h6>$<?php
+                            echo $total;
+                            ?></h6>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+<?php
+    require_once ('components/footeri.php');
+?>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
 </html>
