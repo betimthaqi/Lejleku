632a1c3f564e8080c997e6b050779a9c7cdbc80d
Durajet Mustafa <durajet.mustafa@studentet.uni-pr.edu>
2020-05-17 02:46:03 +0200

    add: logobar

--- aksesor.php
+++ aksesor.php
@@ -46,7 +46,8 @@ if (isset($_POST['add'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Shoppin cart</title>
+    <title>Aksesor</title>
+    <link rel="icon" href="images/logo-bar.png" type="image/x-icon">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/layout.css">
     <link rel="stylesheet" href="css/cart.css">
