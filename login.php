6206b4c8d2cc566e3d7833c824297dbcbf6d79ef
Durajet Mustafa <durajet.mustafa@studentet.uni-pr.edu>
2020-05-17 02:48:11 +0200

    add: logobar

--- login.php
+++ login.php
@@ -98,6 +98,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <head>
     <meta charset="UTF-8">
     <title>Kyqja</title>
+    <link rel="icon" href="images/logo-bar.png" type="image/x-icon">
     <link rel="stylesheet" href="css/stylelogin.css">
     <style type="text/css">
         body {
